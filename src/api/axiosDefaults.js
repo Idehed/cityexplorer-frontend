9d69d08b5b62ac34b83de0f8273fe6bdadb8a5b6
Anton Idehed <anton.idehed@telia.com>
2024-06-28 19:47:36 +0000

    installed axios , created api folder with axiosdefaults.js file in it

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://cityexplorer-api-1e4f09c72732.herokuapp.com/';
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
+axios.defaults.withCredentials = true;
