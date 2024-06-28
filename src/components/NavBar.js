195dcce4ad059f9505cc9ab59b48d419e04f78f8
Anton Idehed <anton.idehed@telia.com>
2024-06-28 19:33:26 +0000

    creating routes for the links in the navbar and adding a contact page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,26 +2,49 @@ import React from 'react'
 import { Navbar, Container, Nav} from "react-bootstrap";
 import logo from "../assets/family.png";
 import styles from '../styles/NavBar.module.css';
+import { NavLink } from "react-router-dom"
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
+        <NavLink to="/">
         <Navbar.Brand className={styles.LogoT}>CityExplorer 
             <img src={logo} alt="logo" height="45" className={styles.LogoM} />
         </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <Nav.Link> 
-              <i class="fa-solid fa-earth-europe"></i> Explore
-            </Nav.Link>
-            <Nav.Link>
-            <i class="fa-solid fa-arrow-right-to-bracket"></i> Sign in
-            </Nav.Link>
-            <Nav.Link>
-            <i class="fa-solid fa-user-plus"></i>Sign up
-            </Nav.Link>
+          <NavLink
+              exact
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/"
+            >
+              <i class="fa-solid fa-earth-europe"></i>Explore
+            </NavLink>
+            <NavLink
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/signin"
+            >
+              <i class="fa-solid fa-arrow-right-to-bracket"></i>Sign in
+            </NavLink>
+            <NavLink
+              to="/signup"
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+            >
+              <i class="fa-solid fa-user-plus"></i>Sign up
+            </NavLink>
+            <NavLink
+              to="/contact"
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+            >
+              <i class="fa-solid fa-envelopes-bulk"></i>Contact
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
