9c0e5f07a7763bc2986460c447c6ce43aba93719
Anton Idehed <anton.idehed@telia.com>
2024-07-03 12:09:09 +0000

    adding 3 tokens and adding it to signupform, currentusercontext and navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,6 +8,7 @@ from '../contexts/CurrentUserContext';
 import Avatar from './Avatar';
 import axios from "axios";
 import useClickOutsideToggle from '../hooks/useClickOutsideToggle';
+import { removeTokenTimestamp } from '../utils/utils';
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
@@ -19,6 +20,7 @@ const NavBar = () => {
     try {
       await axios.post("dj-rest-auth/logout/");
       setCurrentUser(null);
+      removeTokenTimestamp();
     } catch (err) {
       console.log(err);
     }
